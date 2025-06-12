534a6536c58bc35161a34918b05265d209597378
phamhongphuctu <phamtu660@gmail.com>
2025-06-12 14:52:16 -0600

    met qua

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/", // 👈 Cố định base root để hoạt động đúng trong Pi Browser
   plugins: [react()],
 })
